c25c1b2c515748b1ddc31e21eaa265fa37a06222
analisticsdatacompany <analisticsdata.company@gmail.com>
2025-08-06 11:43:15 -0300

    controle do usuario

--- public/device/all.php
+++ public/device/all.php
@@ -1,16 +1,13 @@
 <?php
-
 use App\Libs\Http;
 use App\Libs\Response;
 use App\Midleware\Authenticate;
-
 require_once __DIR__ . "./../../vendor/autoload.php";
-
 try {
     $config = require __DIR__ . '/../../core/config.php';
     Http::cors();
-    $request= Http::all();
-    Authenticate::Init($config,$request,['get-devices']);
+    $request = Http::all();
+    Authenticate::Init($config, $request, ['get-devices']);
     $data = [
         $request
     ];
